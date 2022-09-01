cdbfef74b2ef3807b5c2fb31f94148d49999eb1d
Aristotle609 <62410110+Aristotle609@users.noreply.github.com>
2022-09-01 18:18:21 +0530

    stock changer

--- accounts.py
+++ accounts.py
@@ -18,10 +18,23 @@ dict = {'Date':[datetime.today()],
         'Price_Bought':[price_bought],
         'quantity':[quantity],
         'total_price':[total],
-        "budget":[budget]
+        "budget":[budget],
+        "Current Price":[pd.NA],
+        "Change Percent":[pd.NA]
        }
 
+
 df = pd.DataFrame(dict)
+def current_price_calc(calc):
+    Company = yf.Ticker(calc["Ticker"])
+    return Company.info['currentPrice']
+
+def change_price(price):
+    return (float(price["Current Price"])-float(price["Price_Bought"]))/100
+
 account = pd.read_csv("data/accounts.csv")
 df = df.append(account, ignore_index = True)
+df["Current Price"] = df.apply(current_price_calc,axis = 1)
+df["Change Percent"] = df.apply(change_price,axis = 1)
+
 df.to_csv("data/accounts.csv" , index = False)
